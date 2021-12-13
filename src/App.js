2ba46ffc093a0d52bea09a2da5ab41f361436e07
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2021-12-13 17:42:11 -0500

    Adding github repo link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        <a
+          href="https://github.com/snbarnhouse/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-Source
+        </a>{" "} Code
+        by Stephanie Barnhouse
+      </footer>
     </div>
   );
 }
