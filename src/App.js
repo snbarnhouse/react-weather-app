de7258c566a3377b2ef2c25b9d91b83c1f862e00
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2021-12-13 17:36:09 -0500

    cleaning up initial code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
@@ -9,4 +9,4 @@ function App() {
   );
 }
 
-export default App;
+
