5b9c198f4f0701a30462f1be5aef80f32880cf81
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2021-12-13 19:29:47 -0500

    Adding HTML structure.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-
+import React from "react";
+import Weather from "./Weather";
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <div className= "container">
-      <h1>Weather App</h1>
+      <Weather />
       <footer>
         <a
           href="https://github.com/snbarnhouse/react-weather-app"
---
+++ src/Weather.js
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Search for a city"
+                className="form-control"
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="search" className="btn btn-primary" />
+            </div>
+          </div>
+        </form>
+        <h1>Raleigh</h1>
+        <ul>
+          <li>Monday 7:18</li>
+          <li>Mostly Cloudy</li>
+        </ul>
+        <div className="row">
+          <div className="col-6">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+              alt="icon"
+            />
+            24°F
+          </div>
+          <div className="col-6">
+            <ul>
+              <li>Precipitation: 15%</li>
+              <li>Humidity: 72%</li>
+              <li>Wind: 5mph</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    );
+}
