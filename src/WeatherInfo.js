81e3dd2d382d1f5e5d6f92ef6d75a6b20e57a20d
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2021-12-15 10:55:11 -0500

    Converting from f to c

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,6 +1,7 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
     return (
@@ -19,10 +20,7 @@ export default function WeatherInfo(props) {
                 <WeatherIcon code={props.data.icon} size={52} />
               </div>
               <div className="float-left">
-                <span className="temperature">
-                  {Math.round(props.data.temperature)}
-                </span>
-                <span className="unit">°F</span>
+              <WeatherTemperature fahrenheit={props.data.temperature}/>
               </div>
             </div>
           </div>
---
+++ src/WeatherTemperature.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+export default function WeatherTemperature(props) {
+  const [unit, setUnit] = useState("fahrenheit");
+  function convertToCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  function convertToFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  if (unit === "fahrenheit") {
+    return (
+      <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(props.fahrenheit)}</span>
+        <span className="unit">
+          °F |{" "}
+          <a href="/" onClick={convertToCelsius}>
+            °C
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+      let celsius = (props.fahrenheit - 32) * 5 / 9;
+
+    return (
+      <div className="WeatherTemperature">
+        <span className="temperature">{Math.round(celsius)}</span>
+        <span className="unit">
+          <a href="/" onClick={convertToFahrenheit}>
+            °F
+          </a>{" "}
+          | °C
+        </span>
+      </div>
+    );
+  }
+}
