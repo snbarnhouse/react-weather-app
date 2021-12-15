77a99d3daf439cf36781a5ddd8d90f668f8184fb
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2021-12-15 06:23:26 -0500

    Adding weather icon

--- src/Weather.js
+++ src/Weather.js
@@ -13,8 +13,8 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
-      wind: response.data.wind.speed, 
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      wind: response.data.wind.speed,
       city: response.data.name,
     });
   }
