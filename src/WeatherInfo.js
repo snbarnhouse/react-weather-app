f38b26b6f96e3ed35b7e74d625977884f730f414
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2021-12-15 07:52:12 -0500

    Fixing weather icon/temp

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -14,7 +14,7 @@ export default function WeatherInfo(props) {
         </ul>
         <div className="row mt-3">
           <div className="col-6">
-            <div className="clearfix">
+            <div className="d-flex">
               <div className="float-left">
                 <WeatherIcon code={props.data.icon} size={52} />
               </div>
