afcee92ba9a292863583e234fd0a2cf4251ad84e
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2021-12-20 19:45:42 -0500

    Adding api to future forcast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,20 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
 export default function WeatherForecast() {
+    function handleResponse(response) {
+
+    }
+
+    let apiKey = "1743d71cea491649f0bd96f06af46d71";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=imperial`;
+
+    axios.get(apiUrl).then(handleResponse);
+
     return (
         <div className= "WeatherForecast">
             <div className="row">
