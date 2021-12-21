a2b5c2a55309eba0221b3b5c0e3819201e8f32b6
Stephanie Barnhouse <snbarnhouse@yahoo.com>
2021-12-21 16:12:02 -0500

    Adding return of null

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import WeatherForecastDay from "./WeatherForecastDay";
 import "./WeatherForecast.css";
 import axios from "axios";
-import { cleanup } from "@testing-library/react";
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
@@ -28,6 +27,8 @@ export default function WeatherForecast(props) {
                   <WeatherForecastDay data={dailyForecast} />
                 </div>
               );
+            } else {
+              return null;
             }
           })}
           {/* <div className="col">
